2101c543d4d0d08f0ad02a35f7f4a1e754979a8d
vigneshvaran@brimmatech.com <vigneshvaran@brimmatech.com>
2024-10-30 09:31:43 +0530

    new file added

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String [] args){
         System.out.println("vicky");
         System.out.println("brimma");
-        System.out.println("0000");
+        System.out.println("123");
     }
 }
